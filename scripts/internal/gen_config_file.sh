2a30f6b17a28c8945d0297319e1ab5b4311c7821
nduykha <nguyenkha190806@gmail.com>
2026-06-07 20:46:51 +0700

    Revert "Add A71 workflows & Enable UN1CA Updates"
    This reverts commit 6c706c7b239c4d4b4df7597f250f046a6727f84e.

--- scripts/internal/gen_config_file.sh
+++ scripts/internal/gen_config_file.sh
@@ -27,7 +27,7 @@ IS_UNICA_CERT_AVAILABLE()
     local PLATFORM_KEY_SHA1="5b0eb951718acc596370dabab83f546e779b21dc"
     local OTA_KEY_SHA1="681aa9d28fe5fc60be8c25dc5f26a73ec3d6fb46"
 
-    local USES_UNICA_CERT="true"
+    local USES_UNICA_CERT="false"
     if [[ "$(sha1sum "$SRC_DIR/security/unica_platform.pk8" 2> /dev/null | cut -d " " -f 1)" == "$PLATFORM_KEY_SHA1" ]] && \
             [[ "$(sha1sum "$SRC_DIR/security/unica_ota.pk8" 2> /dev/null | cut -d " " -f 1)" == "$OTA_KEY_SHA1" ]]; then
         USES_UNICA_CERT="true"
